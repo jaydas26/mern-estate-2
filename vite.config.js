7f864275d9d6885c8b67a2e9fd1c603139b0ae4a
Your Name <you@example.com>
2023-10-10 17:27:40 +0530

    complete sign up page functionality

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,15 @@ import react from '@vitejs/plugin-react-swc'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  server: {
+    proxy: {
+      '/api': {
+        target: 'http://localhost:4000',
+        // Set secure to false only if your server runs on HTTP (not HTTPS)
+        secure: false,
+      },
+    },
+  },
+
   plugins: [react()],
 })
